60ac3fcf6fad2b387796ceeb7664b3fa5e8b1045
junjie020 <kitchen.gz.020@gmail.com>
2022-04-13 16:22:59 +0800

    fetch shader compile info from vulkan shader file

--- shaders/make.lua
+++ shaders/make.lua
@@ -11,61 +11,6 @@ local cwd = fs.path(lm.workdir)
 local vulkan_shader_dir = (cwd / lm.EfkDir) / "Effekseer/Dev/Cpp/EffekseerRendererVulkan/EffekseerRenderer/Shader"
 local shader_output_dir = cwd
 
-local shaderfiles = {
-    sprite = {
-        Unlit = {
-            vs      = "sprite_unlit_vs.fx.vert",
-            fs      = "model_unlit_ps.fx.frag",
-        },
-        Lit = {
-            vs      = "sprite_lit_vs.fx.vert",
-            fs      = "model_lit_ps.fx.frag",
-        },
-        BackDistortion = {
-            vs      = "sprite_distortion_vs.fx.vert",
-            fs      = "model_distortion_ps.fx.frag",
-        },
-        AdvancedUnlit = {
-            vs      = "ad_sprite_unlit_vs.fx.vert",
-            fs      = "ad_model_unlit_ps.fx.frag",
-        },
-        AdvancedLit = {
-            vs      = "ad_sprite_lit_vs.fx.vert",
-            fs      = "ad_model_lit_ps.fx.frag",
-        },
-        AdvancedBackDistortion = {
-            vs      = "ad_sprite_distortion_vs.fx.vert",
-            fs      = "ad_model_distortion_ps.fx.frag",
-        },
-    },
-    model = {
-        Unlit = {
-            vs      = "model_unlit_vs.fx.vert",
-            fs      = "model_unlit_ps.fx.frag",
-        },
-        Lit = {
-            vs      = "model_lit_vs.fx.vert",
-            fs      = "model_lit_ps.fx.frag",
-        },
-        BackDistortion = {
-            vs      = "model_distortion_vs.fx.vert",
-            fs      = "model_distortion_ps.fx.frag",
-        },
-        AdvancedUnlit = {
-            vs      = "ad_model_unlit_vs.fx.vert",
-            fs      = "ad_model_unlit_ps.fx.frag",
-        },
-        AdvancedLit = {
-            vs      = "ad_model_lit_vs.fx.vert",
-            fs      = "ad_model_lit_ps.fx.frag",
-        },
-        AdvancedBackDistortion = {
-            vs      = "ad_model_distortion_vs.fx.vert",
-            fs      = "ad_model_distortion_ps.fx.frag",
-        },
-    },
-}
-
 local sc = require "buildscripts.shader_compile"
 
 local function print_cfg(cfg)
@@ -77,70 +22,34 @@ end
 local function cvt2bgfxshader(input, output, shadertype, stage, modeltype)
     lm:build {
         "$luamake", "lua", "@../efkmatc/genbgfxshader.lua", "$in", "$out", shadertype, stage, modeltype, "@../efkmatc",
-        deps = {
-            "efkmat"
-        },
+        deps = "efkmat",
+        description = "Compile sc: $out",
         input = input:string(),
         output = output:string(),
     }
 end
 
-local files_built = {}
+local scfiles = {}
 
-local function build_eff_shader(input, scfile, output, defines, stagetype, shadertype, modeltype)
-    if files_built[input:string()] then
-        return
-    end
-
-    files_built[input:string()] = scfile
-    cvt2bgfxshader(input, scfile, shadertype, stagetype, modeltype)
-
-    local varying_filename = ("%s_%s_varying.def.sc"):format(modeltype, shadertype)
-    local varying_path = scfile:parent_path() / varying_filename
-    local cfg = {
-        stage = stagetype,
-        optimizelevel = 3,
-        debug = true,
-        includes = {
-            cwd / BgfxDir / "src",
-        },
-        defines = defines,
-        varying_path = varying_path:string(),
-        input = scfile:string(),
-        output = output:string(),
-    }
-
-    --print_cfg(cfg)
-    local cmd = sc.gen_cmd(Shaderc:string(), cfg)
-    --print(table.concat(cmd, " "))
-    lm:build(cmd)
-end
-
-local shader_target_files = {
-    inputs = {},
-    scfiles = {},
-    outputs = {},
-}
-
-for modeltype, shaders in pairs(shaderfiles) do
-    for st, shader in pairs(shaders) do
-        for _, stage in ipairs{"vs", "fs"} do
-            local filename = shader[stage]
-            local input = vulkan_shader_dir / filename
-            local scfile = shader_output_dir / fs.path(filename):replace_extension "sc"
-            local output = fs.path(scfile):replace_extension "bin"
-            shader_target_files.inputs[#shader_target_files.inputs+1]   = input:string()
-            shader_target_files.scfiles[#shader_target_files.scfiles+1] = scfile:string()
-            shader_target_files.outputs[#shader_target_files.outputs+1] = output:string()
-            build_eff_shader(input, scfile, output, shader.defines, stage, st, modeltype)
+local function get_shaders_info(src_shaderpath)
+    local ss = {}
+    for fn in fs.pairs(src_shaderpath) do
+        local ext = fn:extension():string():lower()
+        if ext == ".vert" or ext == ".frag" then
+            ss[#ss+1] = ShaderInfoFromFilename(fn:string())
         end
     end
+
+    return ss
+end
+
+for _, s in ipairs(get_shaders_info(vulkan_shader_dir)) do
+    local input = vulkan_shader_dir / s.filename
+    local scfile = shader_output_dir / fs.path(s.filename):replace_extension "sc"
+    scfiles[#scfiles+1] = scfile
+    cvt2bgfxshader(input, scfile, s.shadertype, s.stage, s.modeltype)
 end
 
 lm:phony "efxbgfx_shaders" {
-    input = shader_target_files.scfiles,
-}
-
-lm:phony "efkbgfx_shader_binaries" {
-    input = shader_target_files.outputs,
+    input = scfiles
 }
